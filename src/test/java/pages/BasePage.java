646551dd48c4792b9247c377d6a03dee65bdb16f
Mikail Sonmez <mikail4458@hotmail.com>
2022-10-03 19:33:36 +0300

    v13

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -43,6 +43,7 @@ public abstract class BasePage {
         waitUntilLoaderScreenDisappear();
 //        BrowserUtils.waitForStaleElement(pageSubTitle);
         return pageSubTitle.getText();
+
     }
 
 

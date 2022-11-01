ac0c3b18c5b8a6b4bb30086bc87304bb807c2a2c
Mikail Sonmez <mikail4458@hotmail.com>
2022-11-01 20:21:19 +0300

    v23

--- src/test/java/step_definitions/GeneralInformationPageStepDefs.java
+++ src/test/java/step_definitions/GeneralInformationPageStepDefs.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class GeneralInformationPageStepDefs {
 
     GeneralInformationPage generalInformationPage = new GeneralInformationPage();
+
     VehiclesPage vehiclesPage = new VehiclesPage();
 
     Actions actions = new Actions(Driver.get());
